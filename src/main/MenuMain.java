81e52f66010d06beb0d9df13e2b91b94a10ddaff
Eliabe <00eliabeed1@gmail.com>
2025-04-01 00:15:46 +0200

    GUI actualizado

---
+++ src/main/MenuMain.java
@@ -0,0 +1,16 @@
+package main;
+
+import gui.GUI_Alumno_NotasGeneral;
+import gui.GUI_Login;
+import gui.GUI_Menu;
+import java.awt.Color;
+import javax.swing.*;
+
+public class MenuMain {
+    public static void main(String[] args) {
+        
+//        GUI_Menu menu = new GUI_Menu();
+          GUI_Login gl = new GUI_Login();
+        
+    }
+}
